df1df8d6f0601ab8d0e9c24ac028f62153600a3c
Hoang Tien Thai <129405867+hthai1523@users.noreply.github.com>
2024-09-28 11:29:44 +0700

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
+
+const DynamicPhotoSphereViewer = () => {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    // Lấy giá trị của tham số 'img' từ URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const imgParam = urlParams.get("image");
+
+    if (imgParam) {
+      // Giải mã URL (trong trường hợp nó đã được mã hóa)
+      setImageUrl(decodeURIComponent(imgParam));
+    }
+
+    // Thêm CSS để ẩn thanh điều hướng và phần mờ
+    const style = document.createElement("style");
+    style.textContent = `
+      .psv-navbar {
+        display: none !important;
+      }
+      .psv-panel {
+        background: none !important;
+      }
+    `;
+    document.head.append(style);
+
+    // Cleanup function
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
+
+  if (imageUrl === "") {
+    return (
+      <div>
+        <p>Ảnh không tồn tại</p>
+      </div>
+    );
+  }
+
+  return (
+    <ReactPhotoSphereViewer
+      src={imageUrl}
+      height={"100vh"}
+      width={"100vw"}
+      navbar={[]}
+    />
+  );
+};
+
+export default DynamicPhotoSphereViewer;
