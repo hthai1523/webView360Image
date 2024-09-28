821405b1d028f02aaeb213b98a41503adc67c724
Hoang Tien Thai <129405867+hthai1523@users.noreply.github.com>
2024-09-28 11:32:23 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
 
 const DynamicPhotoSphereViewer = () => {
